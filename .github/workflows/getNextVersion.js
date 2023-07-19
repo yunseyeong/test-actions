26f0eba088568faf80073dc6e1e39087e7e18d49
seyeong yun <seyeong.yun@medit.com>
2023-07-19 14:16:55 +0900

    sfasfasfa

--- .github/workflows/getNextVersion.js
+++ .github/workflows/getNextVersion.js
@@ -1,3 +1,24 @@
-module.exports = ({currentVersion}) => {
-  console.log(currentVersion);
+module.exports = ({github, currentVersion, versionType}) => {
+  console.log(github)
+  console.log(currentVersion, versionType)
+  const version = currentVersion.replace('v', '').split('.')
+  let major = parseInt(version[0], 10)
+  let minor = parseInt(version[1], 10)
+  let patch = parseInt(version[2], 10)
+  switch (versionType) {
+    case 'patch': {
+      patch++
+      break
+    }
+    case 'minor': {
+      patch = 0
+      minor++
+      break
+    }
+    case 'major': {
+      patch = 0
+      minor = 0
+      major++
+    }
+  }
 }
