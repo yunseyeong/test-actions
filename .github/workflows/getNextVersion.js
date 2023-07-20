839f0d2d63cdb1ccee9820cfb98259d7233cf1af
seyeong yun <seyeong.yun@medit.com>
2023-07-20 11:03:02 +0900

    steetsstetse

--- .github/workflows/getNextVersion.js
+++ .github/workflows/getNextVersion.js
@@ -4,14 +4,15 @@ module.exports = async ({github, context, versionType}) => {
     repo: context.repo.repo,
   })
   const draftRelease = releases.data.filter(_ => _.draft)[0]
-
-  console.log('draftRelease!!!', draftRelease)
-
-  const response = await github.rest.repos.getLatestRelease({
+  if (!draftRelease) {
+    console.warn('There is nothing to release')
+    return
+  }
+  const latestReleaseResponse = await github.rest.repos.getLatestRelease({
     owner: context.repo.owner,
     repo: context.repo.repo,
   })
-  const { name, id } = response.data
+  const { name } = latestReleaseResponse.data
   console.log('currentVersion = ', name);
   const version = name.replace('v', '').split('.')
   let major = parseInt(version[0], 10)
@@ -35,13 +36,14 @@ module.exports = async ({github, context, versionType}) => {
   }
   const nextVersion = `v${major}.${minor}.${patch}`
   console.log('nextVersion = ', nextVersion);
+  
   const updateResponse = await github.rest.repos.updateRelease({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    release_id: id,
+    release_id: draftRelease.id,
     name: nextVersion,
     draft: false,
   })
-  console.log(updateResponse.data)
+  console.log(updateResponse)
   return nextVersion
 }
